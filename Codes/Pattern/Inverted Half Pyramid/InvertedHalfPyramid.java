6184333e7e660bb4f0e7c6fdf104370a488e29a2
Vinitsharma29 <vinitsharma291006@gmail.com>
2026-08-18 21:12:39 +0530

    Create InvertedHalfPyramid.java

---
+++ Codes/Pattern/Inverted Half Pyramid/InvertedHalfPyramid.java
@@ -0,0 +1,16 @@
+class InvertedHalfPyramid{
+    public static void main(String[] args) {
+    //Spaces
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            System.out.print(" ");
+        }
+    // Pattern
+    for(int j=0;j<i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+    } 
+    }
+}
